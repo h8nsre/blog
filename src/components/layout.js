eb4a26a5d7a8dd774c455b2b8749ad0ed4ce0a4d
hiepndd <nguyendangdaihiep@gmail.com>
2019-02-10 23:26:19 +0700

    add some content

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
 
-import Header from './header'
-import '../css/style.css'
+import Header from './header';
+import '../css/style.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,23 +18,25 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <div className="flex flex-col font-sans min-h-screen text-grey-darkest">
-        <Header siteTitle={data.site.siteMetadata.title} />
+        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
 
         <div className="flex flex-col flex-1 md:justify-center max-w-xl mx-auto px-4 py-8 md:p-8 w-full">
-          {children}
+          <h1>Hello.</h1>
+          <div>
+            <h1>My name is Hiep.</h1>
+          </div>
+          <div>
+            <span>
+              I am a software engineer.<br />My goal is to be an excellent
+              product engineer, create big things that make people life better.
+              I have skills in coding, especially in algorithms and complexity.
+            </span>
+          </div>
         </div>
 
         <footer className="bg-blue">
-          <div className="flex justify-between max-w-xl mx-auto p-4 md:p-8 text-sm">
-            <p className="text-white">
-              Created by{' '}
-              <a
-                href="https://taylorbryant.blog"
-                className="font-bold no-underline text-white"
-              >
-                Taylor Bryant
-              </a>
-            </p>
+          {/* <div className="flex justify-between max-w-xl mx-auto p-4 md:p-8 text-sm">
+            <p className="text-white">Hiep Nguyen</p>
 
             <p>
               <a
@@ -44,15 +46,15 @@ const Layout = ({ children }) => (
                 GitHub
               </a>
             </p>
-          </div>
+          </div> */}
         </footer>
       </div>
     )}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
